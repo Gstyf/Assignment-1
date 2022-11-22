c31e6229e172380c9ccfe20e5e1eeef4e7d33551
Gstyf <gustaf.styf.4726@student.uu.se>
2022-11-22 16:36:41 +0100

    Second attempt at commit

---
+++ Source/entity.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "myMath.h"
+
+class Entity
+{
+public:
+	Vector2i posOffset = { 20, 20 };
+	Vector2 size = { 15, 45 };
+	int rsize = 10;
+};
---
+++ Source/level.h
@@ -0,0 +1,3 @@
+#pragma once
+
+//World size.
