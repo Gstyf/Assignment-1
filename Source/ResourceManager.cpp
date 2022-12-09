d2719325a8e8f38afbce7917e055c3f44f93f51d
Gstyf <gustaf.styf.4726@student.uu.se>
2022-12-09 23:35:38 +0100

    New level loader system
    This reads characters instead of multi-decimals. Makes creating levels easier. Took a  while to get it working, because I am not very smart.
    
    Also fixed the wall texture.

--- Source/ResourceManager.cpp
+++ Source/ResourceManager.cpp
@@ -1,4 +1,5 @@
 #include "ResourceManager.h"
+#include <fstream>
 
 std::vector<Level> Resources::Levels;
 std::vector<Texture2D> Resources::Textures;
@@ -10,100 +11,171 @@ constexpr char SoundDirectory[] = "./Resources/Sounds/Sounds.txt";
 
 
 
-int GetMultiDecimalIntFromString(int& Iterator, std::string String)
-	{
-	std::string StringToConvert;
-	while (isdigit(String.at(Iterator)))
-		{
-		StringToConvert += String[Iterator];
-		Iterator++;
-		if (Iterator >= String.size()) { break; }
-		}
-	
-	return (stoi(StringToConvert));
-	}
+//int GetMultiDecimalIntFromString(int& Iterator, std::string String)
+//	{
+//	std::string StringToConvert;
+//	while (isdigit(String.at(Iterator)))
+//		{
+//		StringToConvert += String[Iterator];
+//		Iterator++;
+//		if (Iterator >= String.size()) { break; }
+//		}
+//	
+//	return (stoi(StringToConvert));
+//	}
 
 
-Level ConstructLevelFromString(std::string String)
-	{
-	int i = 0;
-	int Count = 0;
-	Entity TempEntity;
-	Level TempLevel;
-
-	for (int i = 0; i < String.size();)
-		{
-		if (isdigit(String.at(i)))
-			{
-			switch (Count)
-				{
-				case (0):
-					{
-					TempEntity.entityType = static_cast<EntityType> (GetMultiDecimalIntFromString(i, String));
-					Count++;
-					break;
-					}
-
-				case (1):
-					{
-					TempEntity.position.x = GetMultiDecimalIntFromString(i, String);
-					Count++;
-					break;
-					}
-
-				case (2):
-					{
-					TempEntity.position.y = GetMultiDecimalIntFromString(i, String);
-					Count = 0;
-					TempLevel.entities.push_back(TempEntity);
-					TempEntity = Entity {};
-					break;
-					}
-				}
-			}
-		else { i++; }
-		}
-
-	return(TempLevel);
-	}
+//Level ConstructLevelFromString(std::string String)
+//{
+//	Entity TempEntity;
+//	Level TempLevel;
+//	int i = 0, y = 0;
+//	int Count = 0; // might not need this one
+//
+//	for (int i = 0; i < String.size();)
+//		{
+//		if (isdigit(String.at(i)))
+//			{
+//			switch (Count)
+//				{
+//				case (0):
+//					{
+//					TempEntity.entityType = static_cast<EntityType> (GetMultiDecimalIntFromString(i, String));
+//					Count++;
+//					break;
+//					}
+//
+//				case (1):
+//					{
+//					TempEntity.position.x = GetMultiDecimalIntFromString(i, String);
+//					Count++;
+//					break;
+//					}
+//
+//				case (2):
+//					{
+//					TempEntity.position.y = GetMultiDecimalIntFromString(i, String);
+//					Count = 0;
+//					TempLevel.entities.push_back(TempEntity);
+//					TempEntity = Entity {};
+//					break;
+//					}
+//				}
+//			}
+//		else { i++; }
+//		}
+//	return(TempLevel);
+//}
 
 
 void Resources::LoadResources()
-	{
+{
 
 	//Load Sound
 	std::string ReadStringSOUND;
 	std::ifstream SoundFile{ SoundDirectory };
 
 	while (std::getline(SoundFile, ReadStringSOUND))
-		{
+	{
 		const char* ReadStringSOUNDchar = ReadStringSOUND.c_str();
 		Sound TempSOUND = LoadSound(ReadStringSOUNDchar);
 		Sounds.push_back(TempSOUND);
-		}
+	}
 	SoundFile.close();
 
 
 	//Load Textures
 	std::string ReadStringTEXTURE;
-	std::ifstream TextureFile {TextureDirectory};
+	std::ifstream TextureFile{ TextureDirectory };
 
 	while (std::getline(TextureFile, ReadStringTEXTURE))
-		{
+	{
 		const char* ReadStringTEXTUREchar = ReadStringTEXTURE.c_str();
 		Texture2D TempTexture = LoadTexture(ReadStringTEXTUREchar);
 		Textures.push_back(TempTexture);
-		}
+	}
 	TextureFile.close();
 
 
 	//Load Levels
-	std::string ReadStringLEVEL;
-	std::ifstream LevelFile {LevelDirectory};
-	
-	while (std::getline(LevelFile, ReadStringLEVEL))
+	std::string level_creation = LoadFileText(LevelDirectory);
+	Level tLevel;
+	Entity tEntity;
+	int x = 0, y = 0;
+	for (char& c : level_creation)
+	{
+		switch (c)
 		{
-		Levels.push_back(ConstructLevelFromString(ReadStringLEVEL));
+		case 'p':
+		{
+			//Entity tEntity;
+			tEntity.position = Vector2i(x, y);
+			tEntity.entityType = EntityType::PLAYER;
+			tEntity.IsPlayer = true;
+			tLevel.entities.push_back(tEntity);
+			x++;
+			break;
 		}
-	LevelFile.close();
+		case '#':
+		{
+			//Entity tEntity;
+			tEntity.position = Vector2i(x, y);
+			tEntity.entityType = EntityType::WALL;
+			tEntity.IsPlayer = false;
+			tLevel.entities.push_back(tEntity);
+			x++;
+			break;
+		}
+		break;
+		case 'b':
+		{
+			//Entity tEntity;
+			tEntity.position = Vector2i(x, y);
+			tEntity.entityType = EntityType::BOX;
+			tEntity.IsPlayer = false;
+			tLevel.entities.push_back(tEntity);
+			x++;
+			break;
+		}
+		break;
+		case 's':
+		{
+			//Entity tEntity;
+			tEntity.position = Vector2i(x, y);
+			tEntity.entityType = EntityType::SWITCH;
+			tEntity.IsPlayer = false;
+			tLevel.entities.push_back(tEntity);
+			x++;
+			break;
+		}
+		break;
+		case '\n':
+		{
+			x = 0;
+			y++;
+			break;
+		}
+		break;
+		case '*':
+		{
+			Levels.push_back(tLevel);
+			tLevel.entities.clear();
+			x = 0; y = -1;
+			break;
+		}
+		break;
+		default:
+			x++;
+			break;
+		}
 	}
+
+	//std::string ReadStringLEVEL;
+	//std::ifstream LevelFile{ LevelDirectory };
+
+	//while (std::getline(LevelFile, ReadStringLEVEL))
+	//{
+	//	Levels.push_back(ConstructLevelFromString(ReadStringLEVEL));
+	//}
+	//LevelFile.close();
+}
