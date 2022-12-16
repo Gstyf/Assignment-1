e076be274d2e2f57aa34c26adbfea8b2c0390820
Gstyf <gustaf.styf.4726@student.uu.se>
2022-12-16 12:25:32 +0100

    Revert "Moved the bool condition and added it to the entitydescription. Changed newlevellogic to work according to description"
    This reverts commit 640a68a4ef67d51b3a762ee91ae9d632c8458e07.

--- Source/ResourceManager.cpp
+++ Source/ResourceManager.cpp
@@ -69,17 +69,17 @@ void ParsEntityDescriptions(char* string)
 								eat_space(&cursor);
 								if (accept_string(&cursor, "movable"))
 								{
-									desc.IsMovable = true;
+									desc.Movable = true;
 								}
 								else if (accept_string(&cursor, "immovable"))
 								{
-									desc.IsMovable = false;
+									desc.Movable = false;
 								}
 							}
 						}
 					}
 					eat_space(&cursor);
-					if (accept_string(&cursor, "Object"))
+					if (accept_string(&cursor, "texture"))
 					{
 						eat_space(&cursor);
 						if (accept_string(&cursor, ":"))
@@ -91,27 +91,18 @@ void ParsEntityDescriptions(char* string)
 								if (accept_string(&cursor, "player"))
 								{
 									desc.Textures = 0;
-									desc.IsPlayer = true;
 								}
 								else if (accept_string(&cursor, "wall"))
 								{
 									desc.Textures = 1;
-						
 								}
 								else if (accept_string(&cursor, "box"))
 								{
 									desc.Textures = 2;
-									desc.IsBox = true;
-									desc.IsPlayer = false;
-									desc.IsSwitch = false;
-									
 								}
 								else if (accept_string(&cursor, "switch"))
 								{
 									desc.Textures = 3;
-									desc.IsSwitch = true;
-									desc.IsPlayer = false;
-									desc.IsBox = false;
 								}
 							}
 						}
@@ -201,7 +192,10 @@ void Resources::LoadResources()
 			tEntity.entityType = EntityType::PLAYER;
 			
 			//Code like this should be handled in Aadis code
-			
+			tEntity.IsPlayer = true;
+			tEntity.IsSwitch = false;
+			tEntity.IsMovable = true;
+			tEntity.IsBox = false;
 			
 			//tLevel.entities.push_back(tEntity);
 
@@ -224,6 +218,10 @@ void Resources::LoadResources()
 				}
 			}
 			tEntity.entityType = EntityType::WALL;
+			tEntity.IsPlayer = false;
+			tEntity.IsSwitch = false;
+			tEntity.IsMovable = false;
+			tEntity.IsBox = false;
 			//tLevel.entities.push_back(tEntity);
 			tLevel.EntetiesLayer1.push_back(tEntity);
 			x++;
@@ -241,6 +239,10 @@ void Resources::LoadResources()
 				}
 			}
 			tEntity.entityType = EntityType::BOX;
+			tEntity.IsPlayer = false;
+			tEntity.IsSwitch = false;
+			tEntity.IsMovable = true;
+			tEntity.IsBox = true;
 			//tLevel.entities.push_back(tEntity);
 			tLevel.EntetiesLayer1.push_back(tEntity);
 			x++;
@@ -258,6 +260,10 @@ void Resources::LoadResources()
 				}
 			}
 			tEntity.entityType = EntityType::SWITCH;
+			tEntity.IsPlayer = false;
+			tEntity.IsSwitch = true;
+			tEntity.IsMovable = false;
+			tEntity.IsBox = false;
 			//tLevel.entities.push_back(tEntity);
 			tLevel.EntetiesLayer0.push_back(tEntity);
 			x++;
--- Source/entity.h
+++ Source/entity.h
@@ -14,13 +14,9 @@ enum class EntityType
 
 struct EntityDescription
 {
-	// the properties of the entities
+	bool Movable = false;		// the properties of the entities
 	int Textures = {};
 	char symbolInLevelFile = 0; // redundant??
-	bool IsPlayer = false; //player only
-	bool IsMovable = false; //box and player
-	bool IsSwitch = false; //switches are not going to be entities in the final version
-	bool IsBox = false;
 };
 
 class Entity
@@ -29,6 +25,9 @@ public:
 	Vector2i position; //all entities have position, so this is not removable
 	EntityDescription* entityDescription; //This is the description that dictates if an object is movoable, and what texture it uses.
 	EntityType entityType; //get rid of?
-	
+	bool IsPlayer; //player only
+	bool IsMovable; //box and player
+	bool IsSwitch; //switches are not going to be entities in the final version
+	bool IsBox;
 	};
 
